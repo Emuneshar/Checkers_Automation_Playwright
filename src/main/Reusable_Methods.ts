d2501a10bc75a0d6912ba409fe271415deaefd1a
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2025-01-26 17:43:38 -0500

    set up variables for MovePiece function

--- src/main/Reusable_Methods.ts
+++ src/main/Reusable_Methods.ts
@@ -4,3 +4,11 @@ export async function click(page: Page, xpath: String, elementName: string){
     console.log("Click on " + elementName)
     await page.locator("xpath="+xpath).click()
 }
+
+export async function MovePiece(page: Page, xpathOrigin: string, xpathDestination: string, index: number){
+    const messageOne = "Select an orange piece to move." // Message to check if we can make the first move
+    const messageTwo = "Make a move"
+    const xpathMessage = "//*[@id = 'message']"
+    let messageText = await captureText(page, xpathMessage, "Captured text for verification")
+    
+}
