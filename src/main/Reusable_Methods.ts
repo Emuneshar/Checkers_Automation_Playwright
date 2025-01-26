c16e488c6fb4831d40ec36d07914ee4cd6d3d6b8
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2025-01-26 17:39:06 -0500

    Created click function

--- src/main/Reusable_Methods.ts
+++ src/main/Reusable_Methods.ts
@@ -1,2 +1,6 @@
 import {test, expect, Page} from "@playwright/test"
 
+export async function click(page: Page, xpath: String, elementName: string){
+    console.log("Click on " + elementName)
+    await page.locator("xpath="+xpath).click()
+}
