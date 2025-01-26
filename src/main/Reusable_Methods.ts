b7c978aa9ca8b823bf5faadeab266b07d7535586
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2025-01-26 17:48:32 -0500

    Created the capture text function

--- src/main/Reusable_Methods.ts
+++ src/main/Reusable_Methods.ts
@@ -1,5 +1,10 @@
 import {test, expect, Page} from "@playwright/test"
 
+export async function captureText(page: Page, xpath: string, elementName: string){
+    console.log("Capture text on "+elementName)
+    let result = await page.locator(xpath).textContent()
+}
+
 export async function click(page: Page, xpath: String, elementName: string){
     console.log("Click on " + elementName)
     await page.locator("xpath="+xpath).click()
@@ -9,6 +14,7 @@ export async function MovePiece(page: Page, xpathOrigin: string, xpathDestinatio
     const messageOne = "Select an orange piece to move." // Message to check if we can make the first move
     const messageTwo = "Make a move"
     const xpathMessage = "//*[@id = 'message']"
-    let messageText = await captureText(page, xpathMessage, "Captured text for verification")
-    
+    let messageText = await captureText(page, xpathMessage, "Captured text for verification") // text used for verification
+
+
 }
